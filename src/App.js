c570010b23e8ca073253b8888e4660a7d87d704f
Ian Adam Bear <ian.adam.bear@gmail.com>
2017-09-02 18:52:43 +0100

    top nav

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom'
-import TopMenu from './components/TopMenu/TopMenu';
-import SideMenu from './components/SideMenu/SideMenu';
-import Candidates from './components/Candidates/Candidates';
-import Skills from './components/Skills/Skills';
+import TopNav from './components/view/topNav/top-nav'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TopMenu />
-        <SideMenu />
-
-        <Link to="/candidates">Candidates Link</Link>
-        <Link to="/skills">Skills Link</Link>
-
-        <Switch>
-          <Route path="/candidates" component={Candidates} />
-          <Route path="/skills" component={Skills} />
-        </Switch>
+        <TopNav />
       </div>
-
     );
   }
 }
---
+++ src/components/view/topNav/top-nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.png';
+import user from './user.png';
+import './top-nav.css'
+
+export default function() {
+  return (
+    <div className="top-nav">
+      <img className="top-nav-logo" alt="talentwire" src={logo} />
+      <Link className=" top-nav-title" to='/'>TalentWire</Link>
+      <Link className=" top-nav-link" to='/talent'>Talent</Link>
+      <Link className=" top-nav-link" to='/skills'>Skills</Link>
+      <Link className="top-nav-user-image" to='/user'>
+        <img className="top-nav-user-image" alt="user iamge" src={user} />
+      </Link>
+    </div>
+  );
+}
