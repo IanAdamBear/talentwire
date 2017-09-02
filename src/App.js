0e5addd5e2a7299660a3b273095a9552ec9d4dd4
Ian Adam Bear <ian.adam.bear@gmail.com>
2017-09-02 20:19:57 +0100

    Main section routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import TopNav from './components/view/topNav/top-nav'
 import './App.css';
+import Router from './router'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopNav />
+        <Router />
       </div>
     );
   }
--- src/router.js
+++ src/router.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+import TalentLayout from './components/layout/talentLayout/talent-layout'
+import SkillsLayout from './components/layout/skillsLayout/skills-layout'
+
+
+export default function() {
+  return (
+    <Switch>
+        <Route path='/talent' component={TalentLayout} />
+        <Route path='/skills' component={SkillsLayout} />
+    </Switch>
+  )
+}
