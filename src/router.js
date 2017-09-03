2fb82ca09815a1844648daa11632e43803da10e8
Ian Adam Bear <ian.adam.bear@gmail.com>
2017-09-03 10:28:06 +0100

    added a dashboard route

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import TalentLayout from './components/layout/talentLayout/talent-layout'
 import SkillsLayout from './components/layout/skillsLayout/skills-layout'
+import DashboardLayout from './components/layout/dashboardLayout/dashboard-layout'
 
 
 export default function() {
   return (
     <Switch>
+        <Route exact path="/" component={DashboardLayout}/>
         <Route path='/talent' component={TalentLayout} />
         <Route path='/skills' component={SkillsLayout} />
     </Switch>
